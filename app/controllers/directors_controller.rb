03ddcde8f3dd8404bcc318d55464dd198d7b289b
VladislavSCB <vladislav.samoylenko@chicagobooth.edu>
2022-10-27 21:23:07 +0000

    video done 123

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,27 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors=Director.all
+    render({:template => "directors_template/index.html.erb"})
+  end
+
+  def wisest
+    @oldest = Director.where.not({:dob =>nil}).order({:dob=> :asc}).at(0)
+  render({:template => "directors_template/eldest.html.erb"})
+  end
+
+  def youngest
+    @youngest = Director.where.not({:dob =>nil}).order({:dob=> :desc}).at(0)
+  render({:template => "directors_template/youngest.html.erb"})
+  end
+
+  def director_details
+    #params
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({:id=>the_id}).at(0)
+    @themovies=Movie.where({:director_id=>the_id})
+  render({:template => "directors_template/show.html.erb"})
+
+  end
+
+end
